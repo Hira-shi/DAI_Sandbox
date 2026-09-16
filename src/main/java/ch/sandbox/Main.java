9ae8401f56905f569b8417fc2f7c63a480fbc9c2
Hira-shi <carreljeremie@gmail.com>
2026-09-16 16:12:47 +0200

    hmmmmm

--- src/main/java/ch/sandbox/Main.java
+++ src/main/java/ch/sandbox/Main.java
@@ -1,13 +1,41 @@
 package ch.sandbox;
 
+abstract class Animal {
+  public abstract void sound();
+
+  public abstract void greets();
+}
+
+class Dog extends Animal {
+  public void sound() {
+    System.out.println("Dog sound");
+  }
+
+  @Override
+  public void greets() {
+    System.out.println("Dog jump on you");
+  }
+}
+
+class Cat extends Animal {
+  public void sound() {
+    System.out.println("Cat sound");
+  }
+
+  @Override
+  public void greets() {
+    System.out.println("Cat doesn't care about you");
+  }
+}
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello and welcome!\n");
-        // Ceci est un test
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
-    }
+  public static void main(String[] args) {
+    Animal animal = new Animal();
+    animal.sound();
+    Animal dog = new Dog();
+    dog.sound();
+    Animal cat = new Cat();
+    cat.sound();
+  }
 }
